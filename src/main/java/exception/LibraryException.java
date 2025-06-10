235c121f25325c200cb7d57bfe3fb35262e4e2ad
gabrieledileo <gabriele.dileo@hinto.com>
2025-06-10 14:35:19 +0200

    refactor(exception): remove unused constructor from LibraryException

--- src/main/java/exception/LibraryException.java
+++ src/main/java/exception/LibraryException.java
@@ -6,8 +6,4 @@ public class LibraryException extends Exception {
     public LibraryException(String message) {
         super(message);
     }
-
-    public LibraryException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
